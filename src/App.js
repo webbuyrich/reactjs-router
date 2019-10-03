9394419680259a5619ba7017fd784b2dfb092a17
webbuyrich <webbuyrich@gmail.com>
2019-10-03 04:47:01 -0700

    added Switch component and exact property

--- src/App.js
+++ src/App.js
@@ -10,14 +10,21 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-        
-        <Route path="/about" component={About} />
-        <Route path="/shop" component={Shop} />        
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />    
+        </Switch>    
       </div>
     </Router>
     
   );
 }
 
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;

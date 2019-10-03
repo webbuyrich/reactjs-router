790f5be2d6ce58e6d13e40237f66b2c721b3821e
webbuyrich <webbuyrich@gmail.com>
2019-10-03 04:53:49 -0700

    imported Link component and added styles to links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import '../App.css';
+import { Link } from 'react-router-dom';
+
 
 export class Nav extends Component {
     render() {
+        const navLinkStyle = {
+            color: 'white',
+            textDecoration: 'none'
+        }
         return (
             <nav>
                 <h1>Nav</h1>
                 <ul className="nav-links">
-                    <li>About</li>
-                    <li>Shop</li>
+                    <Link style={navLinkStyle} to="/about"><li>About</li></Link>
+                    <Link style={navLinkStyle} to="/shop"><li>Shop</li></Link>
                 </ul>
             </nav>
         )

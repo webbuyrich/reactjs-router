eea75b9640a48e6a3d73e8524de78bc09f9158a9
webbuyrich <webbuyrich@gmail.com>
2019-10-03 04:33:06 -0700

    added simple template pages

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Nav</h1>
+                <ul>
+                    <li>About</li>
+                    <li>Shop</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Nav
---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Shop extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Shop</h1>
+            </div>
+        )
+    }
+}
+
+export default Shop

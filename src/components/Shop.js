79597cff68a0f1991eb9a31c4a6e39345f9a663f
webbuyrich <webbuyrich@gmail.com>
2019-10-03 05:30:37 -0700

    fixed udpates

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -8,6 +8,7 @@ function Shop(){
         fetchItems();
     },[]);
 
+
     const fetchItems = async () => {
         const data = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
         const items = await data.json();
